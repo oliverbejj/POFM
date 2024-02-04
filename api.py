aa003e7a2ec0d36aa6f5ad99773ada6af6c74555
Oliver <bejjanioliver@gmail.com>
2024-02-03 19:23:28 -0500

    api base

--- api.py
+++ api.py
@@ -1,6 +1,27 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
 app=FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello":"World"}
+class Post(BaseModel):
+    name: str 
+    material: str 
+    size: str
+            
+my_posts = []
+
+
+
+@app.get("/posts")
+async def get_posts():
+    return {"data":my_posts}
+
+
+@app.post("/posts")
+async def user_input(post:Post):
+    post_dict=post.dict()
+    post_dict['id']=randrange(0,1000000)
+    my_posts.append(post_dict)
+    return {"data":"new post"}
+# name str, material str, Size str
